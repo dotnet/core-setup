ca1f4fcf5ea6940a4b9d99fa8f2a619a65734d37
Davis Goodin <dagood@microsoft.com>
2016-07-22 15:22:04 -0500

    Add "restore_tools" that pulls down a new NuGet version for a workaround.

---
+++ restore_projects/NuGet.CommandLine.XPlat.Runner/Program.cs
@@ -0,0 +1,10 @@
+// Copyright (c) .NET Foundation and contributors. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+using NugetProgram = NuGet.CommandLine.XPlat.Program;
+
+public class Program
+{
+    public static void Main(string[] args) => NugetProgram.Main(args);
+}
