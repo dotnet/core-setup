924cf36b7493657fecca8a8da904819947322905
Gaurav Khanna <gkhanna@microsoft.com>
2016-09-09 08:00:21 -0700

    Remove a comment.

--- build_projects/shared-build-targets-utils/DependencyVersions.cs
+++ build_projects/shared-build-targets-utils/DependencyVersions.cs
@@ -7,7 +7,6 @@ namespace Microsoft.DotNet.Cli.Build
 {
     public class DependencyVersions
     {
-        // TODO: Can these versions be updated by automation?
         public static readonly string CoreCLRVersion = "1.1.0-preview1-24508-06";
         public static readonly string JitVersion = "1.1.0-preview1-24508-06";
         public static readonly string CoreCLRVersion_Latest = "1.1.0-preview1-24508-06";
