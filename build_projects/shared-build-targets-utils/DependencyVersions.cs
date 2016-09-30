1596c055c2725593d2eede84dfd5c5b828b46289
Gaurav Khanna <gkhanna@microsoft.com>
2016-09-30 10:20:58 -0700

    Fix versions of CoreCLR and JIT to be used during the build. Also update HostVersion.

--- build_projects/shared-build-targets-utils/DependencyVersions.cs
+++ build_projects/shared-build-targets-utils/DependencyVersions.cs
@@ -7,9 +7,7 @@ namespace Microsoft.DotNet.Cli.Build
 {
     public class DependencyVersions
     {
-        // TODO: Update these for consuming servicing version of CoreCLR packages
-        //       Also, update CoreCLR package referenced at /Users/gkhanna/Github/gkhanna79/core-setup/pkg/projects/Microsoft.NETCore.App/project.json
-        public static readonly string CoreCLRVersion = "1.0.4";
-        public static readonly string JitVersion = "1.0.4";
+        public static readonly string CoreCLRVersion = "1.0.5-servicing-24530-02";
+        public static readonly string JitVersion = "1.0.5-servicing-24530-02";
     }
 }
