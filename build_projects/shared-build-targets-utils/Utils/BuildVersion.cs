e56bb3a470b3b2f5cb8fd14105c07877649c705d
Gaurav Khanna <gkhanna@microsoft.com>
2017-03-07 17:26:44 -0800

    Move Core-Setup to 1.1.2 servicing release

--- build_projects/shared-build-targets-utils/Utils/BuildVersion.cs
+++ build_projects/shared-build-targets-utils/Utils/BuildVersion.cs
@@ -6,7 +6,7 @@ namespace Microsoft.DotNet.Cli.Build
     {
         public string SimpleVersion => $"{Major}.{Minor}.{Patch}.{CommitCountString}";
         public string VersionSuffix => $"{ReleaseSuffix}-{CommitCountString}";
-        public string NetCoreAppVersion => $"{Major}.{Minor}.{Patch}";
+        public string NetCoreAppVersion => $"{Major}.{Minor}.{Patch}-{VersionSuffix}-00";
         public string ProductionVersion => $"{Major}.{Minor}.{Patch}";
     }
 }
--- build_projects/shared-build-targets-utils/Utils/Monikers.cs
+++ build_projects/shared-build-targets-utils/Utils/Monikers.cs
@@ -10,9 +10,9 @@ namespace Microsoft.DotNet.Cli.Build
     public class Monikers
     {
         public const string SharedFrameworkName = "Microsoft.NETCore.App";
-        public const string SharedFxBrandName = "Microsoft .NET Core 1.1.1 - Runtime";
-        public const string SharedHostBrandName = "Microsoft .NET Core 1.1.1 - Host";
-        public const string HostFxrBrandName = "Microsoft .NET Core 1.1.1 - Host FX Resolver";
+        public const string SharedFxBrandName = "Microsoft .NET Core 1.1.2 - Runtime";
+        public const string SharedHostBrandName = "Microsoft .NET Core 1.1.2 - Host";
+        public const string HostFxrBrandName = "Microsoft .NET Core 1.1.2 - Host FX Resolver";
 
         public static string GetProductMoniker(BuildTargetContext c, string artifactPrefix, string version)
         {
