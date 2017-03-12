5de6ac3fd4d8f38710102703a96cd337051f0fe7
Cyd Haselton <chaselton@gmail.com>
2017-03-12 14:44:57 -0500

    Updated build-android-rootfs.sh script
    Updated build-android-rootfs.sh script from the updated corefx build-android-rootfs.sh script to include arm and NDK 14 support.  Hopefully implements copy of libunwind libs/headers from corefx/cross/android-rootfs if they exist.

--- cross/build-android-rootfs.sh
+++ cross/build-android-rootfs.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+
 usage()
 {
     echo "Creates a toolchain and sysroot used for cross-compiling for Android."
@@ -17,10 +18,14 @@ usage()
     exit 1
 }
 
+#Defaults:  These are the default values that will be used if no arguments are passed to script.  Right now script doesn't support param NDK version
 __ApiLevel=21 # The minimum platform for arm64 is API level 21
 __BuildArch=arm64
 __AndroidArch=aarch64
+__AndroidToolchain=aarch64-linux-android
+__NDK_Version=r14
 
+#From corefx PR16819, adds ARM support
 for i in "$@"
     do
         lowerI="$(echo $i | awk '{print tolower($0)}')"
@@ -32,6 +37,14 @@ for i in "$@"
         arm64)
             __BuildArch=arm64
             __AndroidArch=aarch64
+	    __AndroidToolchain=aarch64-linux-android
+            # Old value...is this correct toolchain for arm64?
+            #__AndroidToolchain=arm-linux-androideabi
+            ;;
+        arm)
+            __BuildArch=arm
+            __AndroidArch=arm
+            __AndroidToolchain=arm-linux-androideabi
             ;;
         *[0-9])
             __ApiLevel=$i
@@ -45,13 +58,14 @@ done
 # Obtain the location of the bash script to figure out where the root of the repo is.
 __CrossDir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+# Set up directories based on root of repo
 __Android_Cross_Dir="$__CrossDir/android-rootfs"
 __NDK_Dir="$__Android_Cross_Dir/android-ndk-r13b"
 __libunwind_Dir="$__Android_Cross_Dir/libunwind"
-__libunwind_cfxdir=__libunwind_clrdir=`sudo find / -wholename "*/coreclr/cross/android-rootfs/toolchain/arm64/sysroot/usr"`
-__lldb_Dir="$__Android_Cross_Dir/lldb"
 __ToolchainDir="$__Android_Cross_Dir/toolchain/$__BuildArch"
+__libunwind_cfxdir=__libunwind_clrdir=`sudo find / -wholename "*/coreclr/cross/android-rootfs/toolchain/arm64/sysroot/usr"`
 
+# Check if environment variables are set
 if [[ -n "$TOOLCHAIN_DIR" ]]; then
     __ToolchainDir=$TOOLCHAIN_DIR
 fi
@@ -64,20 +78,14 @@ echo "Target API level: $__ApiLevel"
 echo "Target architecture: $__BuildArch"
 echo "NDK location: $__NDK_Dir"
 echo "Target Toolchain location: $__ToolchainDir"
+#TODO:  Echo result of check for existing cross-compiled libunwind files in corefx repo (line 65)
 
 # Download the NDK if required
 if [ ! -d $__NDK_Dir ]; then
     echo Downloading the NDK into $__NDK_Dir
     mkdir -p $__NDK_Dir
-    wget -nv -nc --show-progress https://dl.google.com/android/repository/android-ndk-r13b-linux-x86_64.zip -O $__Android_Cross_Dir/android-ndk-r13b-linux-x86_64.zip
-    unzip -q $__Android_Cross_Dir/android-ndk-r13b-linux-x86_64.zip -d $__Android_Cross_Dir
-fi
-
-if [ ! -d $__lldb_Dir ]; then
-    mkdir -p $__lldb_Dir
-    echo Downloading LLDB into $__lldb_Dir
-    wget -nv -nc --show-progress https://dl.google.com/android/repository/lldb-2.3.3614996-linux-x86_64.zip -O $__Android_Cross_Dir/lldb-2.3.3614996-linux-x86_64.zip
-    unzip -q $__Android_Cross_Dir/lldb-2.3.3614996-linux-x86_64.zip -d $__lldb_Dir
+    wget -nv -nc --show-progress https://dl.google.com/android/repository/android-ndk-$__NDK_Version-linux-x86_64.zip -O $__Android_Cross_Dir/android-ndk-$__NDK_Version-linux-x86_64.zip
+    unzip -q $__Android_Cross_Dir/android-ndk-$__NDK_Version-linux-x86_64.zip -d $__Android_Cross_Dir
 fi
 
 # Create the RootFS for both arm64 as well as aarch
@@ -130,8 +138,6 @@ dpkg -x $__Android_Cross_Dir/deb/libcurl-dev_7.52.1_$__AndroidArch.deb $__Androi
 dpkg -x $__Android_Cross_Dir/deb/libcurl_7.52.1_$__AndroidArch.deb $__Android_Cross_Dir/tmp/$__AndroidArch/
 
 #unpacking CoreFX dependencies
-dpkg -x $__Android_Cross_Dir/deb/libcurl-dev_7.52.1_$__AndroidArch.deb $__Android_Cross_Dir/tmp/$__AndroidArch/
-dpkg -x $__Android_Cross_Dir/deb/libcurl_7.52.1_$__AndroidArch.deb $__Android_Cross_Dir/tmp/$__AndroidArch/
 dpkg -x $__Android_Cross_Dir/deb/krb5-dev_1.15_$__AndroidArch.deb $__Android_Cross_Dir/tmp/$__AndroidArch/
 dpkg -x $__Android_Cross_Dir/deb/krb5_1.15_$__AndroidArch.deb $__Android_Cross_Dir/tmp/$__AndroidArch/
 dpkg -x $__Android_Cross_Dir/deb/openssl-dev_1.0.2k_$__AndroidArch.deb $__Android_Cross_Dir/tmp/$__AndroidArch/
@@ -139,17 +145,23 @@ dpkg -x $__Android_Cross_Dir/deb/openssl_1.0.2k_$__AndroidArch.deb $__Android_Cr
 
 cp -R $__Android_Cross_Dir/tmp/$__AndroidArch/data/data/com.termux/files/usr/* $__ToolchainDir/sysroot/usr/
 
+# ifaddrs.h is not available natively Android, but a version of it is available
+# See https://github.com/termux/termux-packages/issues/338 for context
+wget -nv -nc https://raw.githubusercontent.com/qnnnnez/android-ifaddrs/master/include/ifaddrs_single.h -O $__ToolchainDir/sysroot/usr/include/ifaddrs.h
+
 # Prepare libunwind
 
-# If libunwind was built for CoreCLR and sysroot where libunwind installed still exists, copy files from there.
 #TODO: Fix conditionals so that libunwind not builtif files exist
-if [[ -n "$__libunwind_cfxdir" ]]; then
+# Should do 'if corefx libunwind header files and corefx libunwind lib files exist, then copy them to correct location'
+if [[ -d $__libunwind_cfxdir/lib/libunwind* && -d $__libunwind_cfxdir/include/libunwind* ]]; then
+# if [ ! -d $__ToolchainDir/sysroot/usr/lib/libunwind* ]; then
+# if [ ! -d $__ToolchainDir/sysroot/usr/include/libunwind* ]; then
+
     cp -r $__libunwind_cfxdir/lib/libunwind* $__ToolchainDir/sysroot/usr/lib/
     cp -r $__libunwind_cfxdir/include/libunwind* $__ToolchainDir/sysroot/usr/include/
-fi
 
-if [ ! -d $__ToolchainDir/sysroot/usr/lib/libunwind* ]; then
-if [ ! -d $__ToolchainDir/sysroot/usr/include/libunwind* ]; then
+else # clone and build libunwind
+
 # Currently, we clone a fork of libunwind which adds support for Android; once this fork has been
 # merged back in, this script can be updated to use the official libunwind repository.
 # There's also an Android fork of libunwind which is currently not used.
@@ -172,7 +184,7 @@ make install > /dev/null
 # This header file is missing
 cp include/libunwind.h $__ToolchainDir/sysroot/usr/include/
 fi
-fi
+#fi
 
 __RootfsDir="$__ToolchainDir/sysroot"
 if [[ -n "$ROOTFS_DIR" ]]; then
