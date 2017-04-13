65cfbd1a8afc2beb71e9ea150c98a2a6edd1f3f1
Christopher Costa <chcosta@microsoft.com>
2017-04-13 09:43:25 -0700

    print command for debugging

--- tools-local/scripts/dockerrun-as-current-user.sh
+++ tools-local/scripts/dockerrun-as-current-user.sh
@@ -13,5 +13,5 @@ while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symli
   [[ "$SOURCE" != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
 done
 DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
-
+echo "$DIR/docker-as-current-user.sh run $@"
 "$DIR/docker-as-current-user.sh" run "$@"
