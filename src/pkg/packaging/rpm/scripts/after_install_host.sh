9f4185af795e524da86262449c7fc9fac90ac9f7
vivmishra <vivmish@microsoft.com>
2017-05-08 13:02:23 -0700

    Changes for RPM packaging
    Fix restoring to the tool with SdkRestoreCommand
    
    Fix restoring to the tool with SdkRestoreCommand
    
    RPM related changes
    
        pick 065f312 Fix restoring to the tool with SdkRestoreCommand
        pick b2a3711 RPM related changes
        pick 74edd45 RPM changes
        pick 94a4739 Fixed dir.props
    
    RPM changes 2
    
    Fixed dir.props
    
    Changes for RPM build
    
    RPM Change 2
    
    RPM
    
    Fixes
    
    Fixed MS Build Task and targets
    
    Updated Task
    
    Task File renamed
    
    Task updated
    
    Task updated2
    
    Edits for RPM packages
    
    Edits for RPM packages
    
    Edits for RPM packaging
    
    Fixed dir props and json typos
    
    Fixed bugs for build break
    
    Fixed bugs for RpmIntermediatesDir
    
    Temporarily removing the changelog
    
    Temp: FPM parameters change
    
    Temp: FPM cmd change
    
    Temp: FPM cmd change
    
    Fixed bugs
    
    Fixed bugs
    
    Fixed bugs for Templates for HFR and SF
    
    Test URL bug
    
    Add back URL
    
    Remove URL
    
    Updated dependencies for Shared FX
    
    Updated Readme
    
    Update Package name parameter
    
    Fixed the copyright path for FXR and SF
    
    Updated Core-Setup-Linux-BT.json with new build steps
    
    Added directories option for FXR and SF
    
    Added directories option for FXR and SF
    
    Fix official builds to work with crossgen -CreatePDB by using run.cmd to build.  This ensures a VS Developer command prompt is initialized before the build.
    
    Directories related changes
    
    Fixed the doc path
    
    Removed explicit removal of host dirs and man file
    
    Fixed doc path
    
    Fixed FXR doc path bug
    
    Fixed typos and fixed URL
    
    Cleanup of the new task
    
    Clean up of task code
    
    Cleanup of Task
    
    Fixed dir typo

---
+++ src/pkg/packaging/rpm/scripts/after_install_host.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+#
+# Copyright (c) .NET Foundation and contributors. All rights reserved.
+# Licensed under the MIT license. See LICENSE file in the project root for full license information.
+#
+echo "Creating symlink: /usr/bin/dotnet"
+ln -sf "/usr/share/dotnet/dotnet" "/usr/bin/dotnet"
