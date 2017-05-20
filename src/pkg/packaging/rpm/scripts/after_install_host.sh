357646b4f297497cf751398c3b75c39be0647354
vivmishra <vivmish@microsoft.com>
2017-05-19 19:06:54 -0700

    Minor comment fixed in script

--- src/pkg/packaging/rpm/scripts/after_install_host.sh
+++ src/pkg/packaging/rpm/scripts/after_install_host.sh
@@ -3,5 +3,5 @@
 # Copyright (c) .NET Foundation and contributors. All rights reserved.
 # Licensed under the MIT license. See LICENSE file in the project root for full license information.
 #
-echo "Creating symlink: /usr/bin/dotnet"
+echo "Creating dotnet host symbolic link: /usr/bin/dotnet"
 ln -sf "/usr/share/dotnet/dotnet" "/usr/bin/dotnet"
