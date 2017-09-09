b92634ee43a0c04fe03ff3f0091d11619993efbb
Santiago Fernandez <safern@microsoft.com>
2017-09-08 17:27:04 -0700

    Move to non-stable 1.1.4

--- build_projects/shared-build-targets-utils/Utils/Monikers.cs
+++ build_projects/shared-build-targets-utils/Utils/Monikers.cs
@@ -10,9 +10,9 @@ namespace Microsoft.DotNet.Cli.Build
     public class Monikers
     {
         public const string SharedFrameworkName = "Microsoft.NETCore.App";
-        public const string SharedFxBrandName = "Microsoft .NET Core 1.1.3 - Runtime";
-        public const string SharedHostBrandName = "Microsoft .NET Core 1.1.3 - Host";
-        public const string HostFxrBrandName = "Microsoft .NET Core 1.1.3 - Host FX Resolver";
+        public const string SharedFxBrandName = "Microsoft .NET Core 1.1.4 - Runtime";
+        public const string SharedHostBrandName = "Microsoft .NET Core 1.1.4 - Host";
+        public const string HostFxrBrandName = "Microsoft .NET Core 1.1.4 - Host FX Resolver";
 
         public static string GetProductMoniker(BuildTargetContext c, string artifactPrefix, string version)
         {
