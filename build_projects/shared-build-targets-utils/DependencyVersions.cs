919f4c114329d7822a6ab2c848182dfa7cabae81
Santiago Fernandez <safern@microsoft.com>
2017-10-12 18:27:50 -0700

    Mark packages as stable for 1.1.5

--- build_projects/shared-build-targets-utils/DependencyVersions.cs
+++ build_projects/shared-build-targets-utils/DependencyVersions.cs
@@ -7,7 +7,7 @@ namespace Microsoft.DotNet.Cli.Build
 {
     public class DependencyVersions
     {
-        public static readonly string CoreCLRVersion = "1.1.5-servicing-25807-02";
-        public static readonly string JitVersion = "1.1.5-servicing-25807-02";
+        public static readonly string CoreCLRVersion = "1.1.5";
+        public static readonly string JitVersion = "1.1.5";
     }
 }
