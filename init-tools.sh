dc1924f3211f30884d9a5a927d171cea4256438b
Dan Seefeldt <dseefeld@microsoft.com>
2017-11-15 15:53:44 -0600

    Add display_error_message function

--- init-tools.sh
+++ init-tools.sh
@@ -37,6 +37,11 @@ fi
 
 echo "Running: $__scriptpath/init-tools.sh" > $__init_tools_log
 
+display_error_message() {
+    echo "Please check the detailed log that follows." 1>&2
+    cat "$__init_tools_log" 1>&2
+}
+
 # Executes a command and retries if it fails.
 execute_with_retry() {
     local count=0
@@ -118,7 +123,10 @@ if [ ! -e $__BUILD_TOOLS_PATH ]; then
     echo "Restoring BuildTools version $__BUILD_TOOLS_PACKAGE_VERSION..."
     echo "Running: $__DOTNET_CMD restore \"$__INIT_TOOLS_RESTORE_PROJECT\" --no-cache --packages $__PACKAGES_DIR --source $__BUILDTOOLS_SOURCE /p:BuildToolsPackageVersion=$__BUILD_TOOLS_PACKAGE_VERSION" >> $__init_tools_log
     $__DOTNET_CMD restore "$__INIT_TOOLS_RESTORE_PROJECT" --no-cache --packages $__PACKAGES_DIR --source $__BUILDTOOLS_SOURCE /p:BuildToolsPackageVersion=$__BUILD_TOOLS_PACKAGE_VERSION >> $__init_tools_log
-    if [ ! -e "$__BUILD_TOOLS_PATH/init-tools.sh" ]; then echo "ERROR: Could not restore build tools correctly. See '$__init_tools_log' for more details."1>&2; fi
+    if [ ! -e "$__BUILD_TOOLS_PATH/init-tools.sh" ]; then 
+        echo "ERROR: Could not restore build tools correctly."1>&2 
+        display_error_message
+    fi
 fi
 
 echo "Initializing BuildTools..."
@@ -128,7 +136,8 @@ echo "Running: $__BUILD_TOOLS_PATH/init-tools.sh $__scriptpath $__DOTNET_CMD $__
 chmod +x $__BUILD_TOOLS_PATH/init-tools.sh
 $__BUILD_TOOLS_PATH/init-tools.sh $__scriptpath $__DOTNET_CMD $__TOOLRUNTIME_DIR >> $__init_tools_log
 if [ "$?" != "0" ]; then
-    echo "ERROR: An error occured when trying to initialize the tools. Please check '$__init_tools_log' for more details."1>&2
+    echo "ERROR: An error occured when trying to initialize the tools."1>&2
+    display_error_message
     exit 1
 fi
 
