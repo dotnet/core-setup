2b7085fac7aa0144d4a6fd2e91e712ce5e4a6e30
Santiago Fernandez Madero <safern@microsoft.com>
2017-12-11 23:27:46 -0800

    Update CoreFX and CoreClr

--- build_projects/shared-build-targets-utils/DependencyVersions.cs
+++ build_projects/shared-build-targets-utils/DependencyVersions.cs
@@ -7,7 +7,7 @@ namespace Microsoft.DotNet.Cli.Build
 {
     public class DependencyVersions
     {
-        public static readonly string CoreCLRVersion = "1.1.6-servicing-26001-02";
-        public static readonly string JitVersion = "1.1.6-servicing-26001-02";
+        public static readonly string CoreCLRVersion = "1.1.6";
+        public static readonly string JitVersion = "1.1.6";
     }
 }
