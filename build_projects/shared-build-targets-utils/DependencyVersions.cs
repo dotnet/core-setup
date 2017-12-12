5dfafd5293c8775ebe16d711b2283d0f1918c975
Santiago Fernandez Madero <safern@microsoft.com>
2017-12-12 10:58:22 -0600

    Update Corefx and CoreClr and mark packages as stable for 1.0.9 (#3513)
    * Update coreclr and corefx for 1.0.9
    
    * Mark packages as stable for 1.0.9

--- build_projects/shared-build-targets-utils/DependencyVersions.cs
+++ build_projects/shared-build-targets-utils/DependencyVersions.cs
@@ -7,7 +7,7 @@ namespace Microsoft.DotNet.Cli.Build
 {
     public class DependencyVersions
     {
-        public static readonly string CoreCLRVersion = "1.0.11-servicing-26001-02";
-        public static readonly string JitVersion = "1.0.11-servicing-26001-02";
+        public static readonly string CoreCLRVersion = "1.0.11";
+        public static readonly string JitVersion = "1.0.11";
     }
 }
