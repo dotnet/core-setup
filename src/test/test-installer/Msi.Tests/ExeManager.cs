216d60b34d86c7dff792b41307835e1ee96be14e
Driele Neves Ribeiro <t-drneve@microsoft.com>
2018-04-17 11:08:59 -0700

    cleaning up unused namespaces

--- src/test/test-installer/Msi.Tests/ExeManager.cs
+++ src/test/test-installer/Msi.Tests/ExeManager.cs
@@ -3,10 +3,6 @@
 
 using System;
 using System.Diagnostics;
-using System.IO;
-using System.Linq;
-using Microsoft.Deployment.WindowsInstaller;
-using Microsoft.Deployment.WindowsInstaller.Package;
 
 namespace Msi.Tests
 {
