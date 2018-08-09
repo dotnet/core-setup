4cc6ac5f43f48b3a6fd3ab73763ce9f287c05fa2
wtgodbe <wigodbe@microsoft.com>
2018-08-09 15:06:56 -0700

    Fix another error

--- build_projects/dotnet-host-build/PublishTargets.cs
+++ build_projects/dotnet-host-build/PublishTargets.cs
@@ -50,7 +50,7 @@ namespace Microsoft.DotNet.Host.Build
 
         [Target(nameof(PrepareTargets.Init),
         nameof(PublishTargets.InitPublish),
-        nameof(PublishTargets.PublishArtifacts),
+        nameof(PublishTargets.PublishArtifacts))]
         [Environment("PUBLISH_TO_AZURE_BLOB", "1", "true")] // This is set by CI systems
         public static BuildTargetResult Publish(BuildTargetContext c)
         {
