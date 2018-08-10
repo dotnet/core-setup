542adf337c94879ee1ac33852c660fd01ccdb9e5
Santiago Fernandez Madero <safern@microsoft.com>
2018-08-10 16:21:34 -0700

    Bump versions for 1.1.10 (#4448)

--- build_projects/shared-build-targets-utils/DependencyVersions.cs
+++ build_projects/shared-build-targets-utils/DependencyVersions.cs
@@ -7,7 +7,7 @@ namespace Microsoft.DotNet.Cli.Build
 {
     public class DependencyVersions
     {
-        public static readonly string CoreCLRVersion = "1.1.9";
-        public static readonly string JitVersion = "1.1.9";
+        public static readonly string CoreCLRVersion = "1.1.10-servicing-26810-01";
+        public static readonly string JitVersion = "1.1.10-servicing-26810-01";
     }
 }
