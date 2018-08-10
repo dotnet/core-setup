bafa58d8eff49f8aa48ce089aad1151b5dacb2e9
Steve Harter <steveharter@users.noreply.github.com>
2018-08-10 09:22:09 -0500

    Add back check for empty dotnet_root for SDK

--- src/corehost/cli/fxr/sdk_resolver.cpp
+++ src/corehost/cli/fxr/sdk_resolver.cpp
@@ -179,18 +179,20 @@ bool sdk_resolver_t::resolve_sdk_dotnet_path(const pal::string_t& dotnet_root, c
     std::vector<pal::string_t> global_dirs;
     bool multilevel_lookup = multilevel_lookup_enabled();
 
-    // dotnet_root contains DIR_SEPARATOR appended that we need to remove.
-    assert(!dotnet_root.empty());
-    pal::string_t dotnet_root_temp = dotnet_root;
-    remove_trailing_dir_seperator(&dotnet_root_temp);
-
-    hive_dir.push_back(dotnet_root_temp);
+    pal::string_t dotnet_root_temp;
+    if (!dotnet_root.empty())
+    {
+        // dotnet_root contains DIR_SEPARATOR appended that we need to remove.
+        dotnet_root_temp = dotnet_root;
+        remove_trailing_dir_seperator(&dotnet_root_temp);
+        hive_dir.push_back(dotnet_root_temp);
+    }
 
     if (multilevel_lookup && pal::get_global_dotnet_dirs(&global_dirs))
     {
         for (pal::string_t dir : global_dirs)
         {
-            // Avoid duplicate of dotnet_dir_temp
+            // Avoid duplicate of dotnet_root_temp
             if (!pal::are_paths_equal_with_normalized_casing(dir, dotnet_root_temp))
             {
                 hive_dir.push_back(dir);
