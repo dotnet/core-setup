b08d8f74ffc1fd6ffefb97b9d736250c645b725d
wtgodbe <wigodbe@microsoft.com>
2018-08-14 10:24:25 -0700

    Update versions for 1.1.10

--- build_projects/shared-build-targets-utils/DependencyVersions.cs
+++ build_projects/shared-build-targets-utils/DependencyVersions.cs
@@ -7,7 +7,7 @@ namespace Microsoft.DotNet.Cli.Build
 {
     public class DependencyVersions
     {
-        public static readonly string CoreCLRVersion = "1.1.10-servicing-26810-01";
-        public static readonly string JitVersion = "1.1.10-servicing-26810-01";
+        public static readonly string CoreCLRVersion = "1.1.10-servicing-26813-03";
+        public static readonly string JitVersion = "1.1.10-servicing-26813-03";
     }
 }
