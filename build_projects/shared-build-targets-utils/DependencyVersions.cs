6fe114c4f09263fc32dc12ffcccf54344022b31e
William Godbe <wigodbe@microsoft.com>
2018-08-14 10:24:34 -0700

    Update versions for 1.0.13 (#4461)

--- build_projects/shared-build-targets-utils/DependencyVersions.cs
+++ build_projects/shared-build-targets-utils/DependencyVersions.cs
@@ -7,7 +7,7 @@ namespace Microsoft.DotNet.Cli.Build
 {
     public class DependencyVersions
     {
-        public static readonly string CoreCLRVersion = "1.0.15-servicing-26809-03";
-        public static readonly string JitVersion = "1.0.15-servicing-26809-03";
+        public static readonly string CoreCLRVersion = "1.0.15-servicing-26813-01";
+        public static readonly string JitVersion = "1.0.15-servicing-26813-01";
     }
 }
