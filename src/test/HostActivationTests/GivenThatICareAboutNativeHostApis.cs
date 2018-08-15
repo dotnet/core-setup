aae990cfcff4eabd9487ab964b02d8b88ef1d15b
Nick Guerrera <nicholg@microsoft.com>
2018-08-15 12:56:13 -0700

    Test each case individually

--- src/test/HostActivationTests/GivenThatICareAboutNativeHostApis.cs
+++ src/test/HostActivationTests/GivenThatICareAboutNativeHostApis.cs
@@ -7,6 +7,8 @@ using System.Runtime.InteropServices;
 using Xunit;
 using FluentAssertions;
 using Microsoft.DotNet.CoreSetup.Test;
+using System.Collections.Generic;
+using Microsoft.DotNet.Cli.Build;
 
 namespace Microsoft.DotNet.CoreSetup.Test.HostActivation.NativeHostApis
 {
@@ -90,77 +92,69 @@ namespace Microsoft.DotNet.CoreSetup.Test.HostActivation.NativeHostApis
                 // Todo: add a native host test app so we can verify this behavior.
                 .NotHaveStdErrContaining("Waiting for breadcrumb thread to exit...");
         }
-        
-         [Fact]
-         public void Hostfxr_resolve_sdk2_and_hostfxr_get_available_sdks_work()
-         {
-            var fixture = sharedTestState.PreviouslyPublishedAndRestoredPortableApiTestProjectFixture.Copy();
-            var dotnet = fixture.BuiltDotnet;
-            var appDll = fixture.TestProject.AppDll;
 
-            string[] globalSdks = new[] { "4.5.6", "1.2.3", "2.3.4-preview" };
-            string[] localSdks = new[] { "0.1.2", "5.6.7-preview", "1.2.3" };
+        private class SdkResolutionFixture
+        {
+            private readonly TestProjectFixture _fixture;
 
-            var exeDir = Path.Combine(fixture.TestProject.ProjectDirectory, "ed");
-            var programFiles = Path.Combine(exeDir, "pf");
-            var workingDir= Path.Combine(fixture.TestProject.ProjectDirectory, "wd");
-            var globalSdkDir = Path.Combine(programFiles, "dotnet", "sdk");
-            var localSdkDir = Path.Combine(exeDir, "sdk");
+            public DotNetCli Dotnet => _fixture.BuiltDotnet;
+            public string AppDll => _fixture.TestProject.AppDll;
+            public string ExeDir => Path.Combine(_fixture.TestProject.ProjectDirectory, "ed");
+            public string ProgramFiles => Path.Combine(ExeDir, "pf");
+            public string WorkingDir => Path.Combine(_fixture.TestProject.ProjectDirectory, "wd");
+            public string GlobalSdkDir => Path.Combine(ProgramFiles, "dotnet", "sdk");
+            public string LocalSdkDir => Path.Combine(ExeDir, "sdk");
+            public string GlobalJson => Path.Combine(WorkingDir, "global.json");
+            public string[] GlobalSdks = new[] { "4.5.6", "1.2.3", "2.3.4-preview" };
+            public string[] LocalSdks = new[] { "0.1.2", "5.6.7-preview", "1.2.3" };
 
-            // start with an empty global.json, it will be ignored, but prevent one lying on disk 
-            // on a given machine from impacting the test.
-            var globalJson = Path.Combine(workingDir, "global.json");
-            Directory.CreateDirectory(workingDir);
-            File.WriteAllText(globalJson, "{}");
-
-            foreach (string sdk in globalSdks)
+            public SdkResolutionFixture(SharedTestState state)
             {
-                Directory.CreateDirectory(Path.Combine(globalSdkDir, sdk));
-            }
+                _fixture = state.PreviouslyPublishedAndRestoredPortableApiTestProjectFixture.Copy();
 
-            foreach (string sdk in localSdks)
-            {
-                Directory.CreateDirectory(Path.Combine(localSdkDir, sdk));
-            }
+                Directory.CreateDirectory(WorkingDir);
 
-            var dotnetLocation = Path.Combine(dotnet.BinPath, $"dotnet{fixture.ExeExtension}");
-            string expectedList;
+                // start with an empty global.json, it will be ignored, but prevent one lying on disk 
+                // on a given machine from impacting the test.
+                File.WriteAllText(GlobalJson, "{}");
 
-            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows)) // multilevel lookup does not exist otherwise
-            {
-                // With multi-level lookup (windows onnly): get local and global sdks sorted by ascending version,
-                // with global sdk coming before local sdk when versions are equal
-                expectedList = string.Join(';', new[]
+                foreach (string sdk in GlobalSdks)
                 {
-                     Path.Combine(localSdkDir, "0.1.2"),
-                     Path.Combine(globalSdkDir, "1.2.3"),
-                     Path.Combine(localSdkDir, "1.2.3"),
-                     Path.Combine(globalSdkDir, "2.3.4-preview"), 
-                     Path.Combine(globalSdkDir, "4.5.6"),
-                     Path.Combine(localSdkDir, "5.6.7-preview"),
-                });
-                dotnet.Exec(appDll, new[] { "hostfxr_get_available_sdks", exeDir } )
-                    .EnvironmentVariable("TEST_MULTILEVEL_LOOKUP_PROGRAM_FILES", programFiles)
-                    .CaptureStdOut()
-                    .CaptureStdErr()
-                    .Execute()
-                    .Should()
-                    .Pass()
-                    .And
-                    .HaveStdOutContaining("hostfxr_get_available_sdks:Success")
-                    .And
-                    .HaveStdOutContaining($"hostfxr_get_available_sdks sdks:[{expectedList}]");
-            }
+                    Directory.CreateDirectory(Path.Combine(GlobalSdkDir, sdk));
+                }
 
-            // Without multi-level lookup: get only sdks sorted by ascending version
-            expectedList = string.Join(';', new[]
+                foreach (string sdk in LocalSdks)
+                {
+                    Directory.CreateDirectory(Path.Combine(LocalSdkDir, sdk));
+                }
+            } 
+        }
+
+        [Fact]
+        public void Hostfxr_get_available_sdks_with_multilevel_lookup()
+        {
+            if (!RuntimeInformation.IsOSPlatform(OSPlatform.Windows)) 
             {
-                 Path.Combine(localSdkDir, "0.1.2"),
-                 Path.Combine(localSdkDir, "1.2.3"),
-                 Path.Combine(localSdkDir, "5.6.7-preview"),
+                // multilevel lookup is not supported on non-Windows
+                return;
+            }
+            
+            var f = new SdkResolutionFixture(sharedTestState);
+
+            // With multi-level lookup (windows onnly): get local and global sdks sorted by ascending version,
+            // with global sdk coming before local sdk when versions are equal
+            string expectedList = string.Join(';', new[]
+            {
+                Path.Combine(f.LocalSdkDir, "0.1.2"),
+                Path.Combine(f.GlobalSdkDir, "1.2.3"),
+                Path.Combine(f.LocalSdkDir, "1.2.3"),
+                Path.Combine(f.GlobalSdkDir, "2.3.4-preview"),
+                Path.Combine(f.GlobalSdkDir, "4.5.6"),
+                Path.Combine(f.LocalSdkDir, "5.6.7-preview"),
             });
 
-            dotnet.Exec(appDll, new[] { "hostfxr_get_available_sdks", exeDir })
+            f.Dotnet.Exec(f.AppDll, new[] { "hostfxr_get_available_sdks", f.ExeDir })
+                .EnvironmentVariable("TEST_MULTILEVEL_LOOKUP_PROGRAM_FILES", f.ProgramFiles)
                 .CaptureStdOut()
                 .CaptureStdErr()
                 .Execute()
@@ -170,14 +164,47 @@ namespace Microsoft.DotNet.CoreSetup.Test.HostActivation.NativeHostApis
                 .HaveStdOutContaining("hostfxr_get_available_sdks:Success")
                 .And
                 .HaveStdOutContaining($"hostfxr_get_available_sdks sdks:[{expectedList}]");
+        }
 
+        [Fact]
+        public void Hostfxr_get_available_sdks_without_multilevel_lookup()
+        {
+            // Without multi-level lookup: get only sdks sorted by ascending version
+
+            var f = new SdkResolutionFixture(sharedTestState);
+
+            string expectedList = string.Join(';', new[]
+            {
+                 Path.Combine(f.LocalSdkDir, "0.1.2"),
+                 Path.Combine(f.LocalSdkDir, "1.2.3"),
+                 Path.Combine(f.LocalSdkDir, "5.6.7-preview"),
+            });
+
+            f.Dotnet.Exec(f.AppDll, new[] { "hostfxr_get_available_sdks", f.ExeDir })
+                .CaptureStdOut()
+                .CaptureStdErr()
+                .Execute()
+                .Should()
+                .Pass()
+                .And
+                .HaveStdOutContaining("hostfxr_get_available_sdks:Success")
+                .And
+                .HaveStdOutContaining($"hostfxr_get_available_sdks sdks:[{expectedList}]");
+        }
+
+        [Fact]
+        public void Hostfxr_resolve_sdk2_without_global_json_or_flags()
+        {
             // with no global.json and no flags, pick latest SDK
-            var expectedData = string.Join(';', new[] 
+
+            var f = new SdkResolutionFixture(sharedTestState);
+
+            string expectedData = string.Join(';', new[]
             {
-                ("resolved_sdk_dir", Path.Combine(localSdkDir, "5.6.7-preview")),
+                ("resolved_sdk_dir", Path.Combine(f.LocalSdkDir, "5.6.7-preview")),
             });
 
-            dotnet.Exec(appDll, new[] { "hostfxr_resolve_sdk2", exeDir, workingDir, "0" })
+            f.Dotnet.Exec(f.AppDll, new[] { "hostfxr_resolve_sdk2", f.ExeDir, f.WorkingDir, "0" })
                 .CaptureStdOut()
                 .CaptureStdErr()
                 .Execute()
@@ -187,14 +214,21 @@ namespace Microsoft.DotNet.CoreSetup.Test.HostActivation.NativeHostApis
                 .HaveStdOutContaining("hostfxr_resolve_sdk2:Success")
                 .And
                 .HaveStdOutContaining($"hostfxr_resolve_sdk2 data:[{expectedData}]");
+        }
 
-            // without global.json and disallowing previews, pick latest non-preview
-            expectedData = string.Join(';', new[]
+        [Fact]
+        public void Hostfxr_resolve_sdk2_without_global_json_and_disallowing_previews()
+        {
+            // Without global.json and disallowing previews, pick latest non-preview
+
+            var f = new SdkResolutionFixture(sharedTestState);
+
+            string expectedData = string.Join(';', new[]
             {
-                ("resolved_sdk_dir", Path.Combine(localSdkDir, "1.2.3"))
+                ("resolved_sdk_dir", Path.Combine(f.LocalSdkDir, "1.2.3"))
             });
 
-            dotnet.Exec(appDll, new[] { "hostfxr_resolve_sdk2", exeDir, workingDir, "disallow_prerelease" })
+            f.Dotnet.Exec(f.AppDll, new[] { "hostfxr_resolve_sdk2", f.ExeDir, f.WorkingDir, "disallow_prerelease" })
                 .CaptureStdOut()
                 .CaptureStdErr()
                 .Execute()
@@ -204,18 +238,25 @@ namespace Microsoft.DotNet.CoreSetup.Test.HostActivation.NativeHostApis
                 .HaveStdOutContaining("hostfxr_resolve_sdk2:Success")
                 .And
                 .HaveStdOutContaining($"hostfxr_resolve_sdk2 data:[{expectedData}]");
+        }
 
-            // Without global.json and disallowing previews, roll forward to preview 
+        [Fact]
+        public void Hostfxr_resolve_sdk2_with_global_json_and_disallowing_previews()
+        {
+            // With global.json specifying a preview, roll forward to preview 
             // since flag has no impact if global.json specifies a preview.
             // Also check that global.json that impacted resolution is reported.
-            File.WriteAllText(globalJson, "{ \"sdk\": { \"version\": \"5.6.6-preview\" } }");
-            expectedData = string.Join(';', new[]
+
+            var f = new SdkResolutionFixture(sharedTestState);
+
+            File.WriteAllText(f.GlobalJson, "{ \"sdk\": { \"version\": \"5.6.6-preview\" } }");
+            string expectedData = string.Join(';', new[]
             {
-                ("resolved_sdk_dir", Path.Combine(localSdkDir, "5.6.7-preview")),
-                ("global_json_path", globalJson),
+                ("resolved_sdk_dir", Path.Combine(f.LocalSdkDir, "5.6.7-preview")),
+                ("global_json_path", f.GlobalJson),
             });
 
-            dotnet.Exec(appDll, new[] { "hostfxr_resolve_sdk2", exeDir, workingDir, "disallow_prerelease" })
+            f.Dotnet.Exec(f.AppDll, new[] { "hostfxr_resolve_sdk2", f.ExeDir, f.WorkingDir, "disallow_prerelease" })
                 .CaptureStdOut()
                 .CaptureStdErr()
                 .Execute()
