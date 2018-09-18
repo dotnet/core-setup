ec439987a24b32ed805954ec22f32c28af4aa422
wtgodbe <wigodbe@microsoft.com>
2018-09-18 15:04:55 -0700

    Bump version to 1.0.14 & Mark packages non-stable

--- build_projects/shared-build-targets-utils/Utils/Monikers.cs
+++ build_projects/shared-build-targets-utils/Utils/Monikers.cs
@@ -10,9 +10,9 @@ namespace Microsoft.DotNet.Cli.Build
     public class Monikers
     {
         public const string SharedFrameworkName = "Microsoft.NETCore.App";
-        public const string SharedFxBrandName = "Microsoft .NET Core 1.0.13 - Runtime";
-        public const string SharedHostBrandName = "Microsoft .NET Core 1.0.13 - Host";
-        public const string HostFxrBrandName = "Microsoft .NET Core 1.0.13 - Host FX Resolver";
+        public const string SharedFxBrandName = "Microsoft .NET Core 1.0.14 - Runtime";
+        public const string SharedHostBrandName = "Microsoft .NET Core 1.0.14 - Host";
+        public const string HostFxrBrandName = "Microsoft .NET Core 1.0.14 - Host FX Resolver";
 
         public static string GetProductMoniker(BuildTargetContext c, string artifactPrefix, string version)
         {
