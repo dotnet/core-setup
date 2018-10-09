daaded467da45bb8e9c35a640318a5489c0371c8
Steve MacLean <Steve.MacLean@microsoft.com>
2018-10-09 12:56:33 -0400

    Respond to feedback

--- Documentation/building/Running-Tests-In-VisualStudio-IDE.md
+++ Documentation/building/Running-Tests-In-VisualStudio-IDE.md
@@ -6,14 +6,14 @@ There are several environment variables which must be set to get this to work co
 
 ## Steps
 
-1 `build.cmd`
-2 `visual-studio-devenv.cmd`
-3 Open the test explorer window within the Visual Studio IDE
-4 Select tests and run and/or debug.
+1. `build.cmd`
+2. `visual-studio-devenv.cmd`
+3. Open the test explorer window within the Visual Studio IDE
+4. Select tests and run and/or debug.
 
 ## Limitations
 
-* The script is not yet designed to be a robust solution.  As test configurastions change this file could get out of date.  The required configuration can be discovered by carefully examining the logs generated when running the tests with `build.cmd -MsBuildLogging=/bl`.  The script can then be updated.
+* The script is not yet designed to be a robust solution.  As test configurations change this file could get out of date.  The required configuration can be discovered by carefully examining the logs generated when running the tests with `build.cmd -MsBuildLogging=/bl`.  The script can then be updated.
 * The script is hardcoded to use the x64 debug build
 
 
--- visual-studio-devenv.cmd
+++ visual-studio-devenv.cmd
@@ -12,7 +12,7 @@ if exist %_VSWHERE% (
 if not exist "%_VSCOMNTOOLS%" set _VSCOMNTOOLS=%VS140COMNTOOLS%
 if not exist "%_VSCOMNTOOLS%" (
     echo Error: Visual Studio 2015 or 2017 required.
-    echo        Please see https://github.com/dotnet/corefx/blob/master/Documentation/project-docs/developer-guide.md for build instructions.
+    echo        Please see https://github.com/dotnet/core-setup/blob/master/Documentation/building/windows-instructions.md for build instructions.
     exit /b 1
 )
 
@@ -21,7 +21,8 @@ call "%_VSCOMNTOOLS%\VsDevCmd.bat"
 
 :Run
 
-:: this makes test explorer work in Visual Studio
+:: Makes test explorer work in Visual Studio by setting up environment variables for tests
+:: See EnvironmentVariables around https://github.com/dotnet/core-setup/blob/master/src/test/dir.proj#L93
 :: Found the env vars by running build -MsBuildLogging=/bl, then look at the Exec task in RunTest target
 
 set CMD_START_DIR=%~dp0
@@ -33,15 +34,15 @@ set BUILDRID=win-x64
 set BUILD_ARCHITECTURE=x64
 set BUILD_CONFIGURATION=Debug
 
-set TEST_ARTIFACTS=%CMD_START_DIR%Bin\tests\%TEST_TARGETRID%.%BUILD_CONFIGURATION%\
+set TEST_ARTIFACTS=%CMD_START_DIR%bin\tests\%TEST_TARGETRID%.%BUILD_CONFIGURATION%\
 set MNA_TFM=netcoreapp3.0
-set MNA_SEARCH=%CMD_START_DIR%Bin\obj\%TEST_TARGETRID%.%BUILD_CONFIGURATION%\hostFxr\host\fxr\*
+set MNA_SEARCH=%CMD_START_DIR%bin\obj\%TEST_TARGETRID%.%BUILD_CONFIGURATION%\sharedFrameworkPublish\shared\Microsoft.NETCore.App\*
 
 :: We expect one hostfxr version directory in the MNA_SEARCH path
 for /d  %%i in (%MNA_SEARCH%) do (
   set MNA_PATH=%%i
   )
 
-set MNA_VERSION=%MNA_PATH:*host\fxr\=%
+set MNA_VERSION=%MNA_PATH:*sharedFrameworkPublish\shared\Microsoft.NETCore.App\=%
 
 devenv %CMD_START_DIR%\Microsoft.DotNet.CoreSetup.sln
