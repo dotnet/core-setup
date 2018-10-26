cdda072b7e7bdf373e7642e699cb0abeb790bf14
vitek-karas <vitek.karas@microsoft.com>
2018-10-26 07:22:42 -0700

    Add a comment about inability to test apphost for now.

--- src/test/HostActivationTests/GivenThatICareAboutWindowsOsShims.cs
+++ src/test/HostActivationTests/GivenThatICareAboutWindowsOsShims.cs
@@ -25,6 +25,9 @@ namespace Microsoft.DotNet.CoreSetup.Test.HostActivation.WindowsOsShims
                 .And.HaveStdOutContaining("Reported OS version is newer or equal to the true OS version - no shims.");
         }
 
+        // Testing the standalone version (apphost) would require to make a copy of the entire SDK
+        // and overwrite the apphost.exe in it. Currently this is just too expensive for one test (160MB of data).
+
         public class SharedTestState : IDisposable
         {
             private static RepoDirectoriesProvider RepoDirectories { get; set; }
