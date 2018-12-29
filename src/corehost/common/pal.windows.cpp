49f8b7441e0aef300aa8087cb6f2ad4b668a3d9f
jbeisner <jbeisner@microsoft.com>
2018-12-29 00:05:52 +0000

    Remove 'get_sdk_self_registered_dir' from the PAL header.

--- src/corehost/common/pal.windows.cpp
+++ src/corehost/common/pal.windows.cpp
@@ -200,28 +200,6 @@ bool pal::get_default_servicing_directory(string_t* recv)
     return true;
 }
 
-bool pal::get_global_dotnet_dirs(std::vector<pal::string_t>* dirs)
-{
-    pal::string_t default_dir;
-    pal::string_t custom_dir;
-    bool dir_found = false;
-    if (get_sdk_self_registered_dir(&custom_dir))
-    {
-        dirs->push_back(custom_dir);
-        dir_found = true;
-    }
-    if (get_default_installation_dir(&default_dir))
-    {
-        // Avoid duplicate global dirs.
-        if (!dir_found || !pal::are_paths_equal_with_normalized_casing(custom_dir, default_dir))
-        {
-            dirs->push_back(default_dir);
-            dir_found = true;
-        }
-    }
-    return dir_found;
-}
-
 bool pal::get_default_installation_dir(pal::string_t* recv)
 {
     pal::char_t* program_files_dir;
@@ -244,7 +222,7 @@ bool pal::get_default_installation_dir(pal::string_t* recv)
     return true;
 }
 
-bool pal::get_sdk_self_registered_dir(pal::string_t* recv)
+bool get_sdk_self_registered_dir(pal::string_t* recv)
 {
 #if !defined(_TARGET_AMD64_) && !defined(_TARGET_X86_)
     //  Self-registered SDK installation directory is only supported for x64 and x86 architectures.
@@ -253,10 +231,10 @@ bool pal::get_sdk_self_registered_dir(pal::string_t* recv)
     recv->clear();
 
     //  ***Used only for testing***
-    pal::string_t dir;
-    if (pal::getenv(_X("_DOTNET_TEST_SDK_SELF_REGISTERED_DIR"), &dir))
+    pal::string_t environmentOverride;
+    if (pal::getenv(_X("_DOTNET_TEST_SDK_SELF_REGISTERED_DIR"), &environmentOverride))
     {
-        recv->assign(dir);
+        recv->assign(environmentOverride);
         return true;
     }
     //  ***************************
@@ -287,6 +265,28 @@ bool pal::get_sdk_self_registered_dir(pal::string_t* recv)
 
     return true;
 #endif
+}
+
+bool pal::get_global_dotnet_dirs(std::vector<pal::string_t>* dirs)
+{
+    pal::string_t default_dir;
+    pal::string_t custom_dir;
+    bool dir_found = false;
+    if (get_sdk_self_registered_dir(&custom_dir))
+    {
+        dirs->push_back(custom_dir);
+        dir_found = true;
+    }
+    if (get_default_installation_dir(&default_dir))
+    {
+        // Avoid duplicate global dirs.
+        if (!dir_found || !are_paths_equal_with_normalized_casing(custom_dir, default_dir))
+        {
+            dirs->push_back(default_dir);
+            dir_found = true;
+        }
+    }
+    return dir_found;
 }
 
 // To determine the OS version, we are going to use RtlGetVersion API
