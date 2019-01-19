402646aab8b164c76d0cfe95a2f76986c589a8f6
Ahson Khan <ahkha@microsoft.com>
2019-01-19 06:03:05 -0800

    Update core-setup local tools to use S.T.Json for netstandard2.0 instead of Newtonsoft.

---
+++ tools-local/tasks/ChangeEntryPointLibraryName.Netstandard2_0.cs
@@ -0,0 +1,245 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using Microsoft.Build.Framework;
+using System.IO;
+using System.Text.Json;
+using System.Diagnostics;
+
+namespace Microsoft.DotNet.Build.Tasks
+{
+    public class ChangeEntryPointLibraryName : BuildTask
+    {
+        [Required]
+        public string DepsFile { get; set; }
+
+        public string NewName { get; set; }
+
+        private string _version;
+
+        public override bool Execute()
+        {
+            bool retVal = false;
+
+            byte[] utf8Json = File.ReadAllBytes(DepsFile);
+
+            // TODO: Replace the use of the Utf8JsonReader with the JsonDocument once it is available.
+
+            var reader = new Utf8JsonReader(utf8Json, isFinalBlock: true, state: default);
+
+            using (var filestream = new FileStream(DepsFile, FileMode.Create, FileAccess.Write))
+            {
+                using (var bufferWriter = new StreamBufferWriter(filestream))
+                {
+                    var state = new JsonWriterState(options: new JsonWriterOptions { Indented = true });
+                    var jsonWriter = new Utf8JsonWriter(bufferWriter, state);
+
+                    retVal = reader.Read();
+
+                    if (retVal)
+                    {
+                        Debug.Assert(reader.TokenType == JsonTokenType.StartObject);
+                        jsonWriter.WriteStartObject();
+                        ReadWriteObject(ref reader, ref jsonWriter);
+                        jsonWriter.Flush();
+                    }
+                }
+            }
+
+            return retVal;
+        }
+
+        internal static void Skip(ref Utf8JsonReader jsonReader)
+        {
+            int depth = jsonReader.CurrentDepth;
+            if (jsonReader.TokenType == JsonTokenType.PropertyName)
+            {
+                jsonReader.Read();
+            }
+
+            if (jsonReader.TokenType == JsonTokenType.StartObject || jsonReader.TokenType == JsonTokenType.StartArray)
+            {
+                while (jsonReader.Read() && depth < jsonReader.CurrentDepth)
+                {
+                }
+            }
+        }
+
+        internal static Exception CreateUnexpectedException(ref Utf8JsonReader reader, string expected)
+        {
+            return new FormatException($"Unexpected character encountered, excepted '{expected}' " +
+                                       $"at line {reader.CurrentState._lineNumber} position {reader.CurrentState._bytePositionInLine}");
+        }
+
+        private void ReadWriteObject(ref Utf8JsonReader reader, ref Utf8JsonWriter writer)
+        {
+            while (reader.Read())
+            {
+                switch (reader.TokenType)
+                {
+                    case JsonTokenType.PropertyName:
+                        ReadWritePropertyName(reader.GetStringValue(), ref reader, ref writer);
+                        break;
+                    case JsonTokenType.EndObject:
+                        writer.WriteEndObject();
+                        break;
+                }
+            }
+        }
+
+        private void ReadWritePropertyName(string propertyName, ref Utf8JsonReader reader, ref Utf8JsonWriter writer)
+        {
+            if (propertyName == "targets")
+            {
+                ReadWritePropertyNameTargets(ref reader, ref writer);
+            }
+            else if (propertyName == "libraries" && !string.IsNullOrEmpty(_version))
+            {
+                ReadWritePropertyNameLibraries(ref reader, ref writer);
+            }
+            else
+            {
+                if (reader.Read())
+                {
+                    switch (reader.TokenType)
+                    {
+                        case JsonTokenType.True:
+                            writer.WriteBoolean(propertyName, true);
+                            break;
+                        case JsonTokenType.False:
+                            writer.WriteBoolean(propertyName, false);
+                            break;
+                        case JsonTokenType.Number:
+                            throw CreateUnexpectedException(ref reader, "Expect non-numeric JSON tokens only within deps.json.");
+                        case JsonTokenType.String:
+                            writer.WriteString(propertyName, reader.ValueSpan);
+                            break;
+                        case JsonTokenType.Null:
+                            writer.WriteNull(propertyName);
+                            break;
+                        case JsonTokenType.StartObject:
+                            writer.WriteStartObject(propertyName);
+                            ReadWriteObject(ref reader, ref writer);
+                            break;
+                        case JsonTokenType.StartArray:
+                            writer.WriteStartArray(propertyName);
+                            ReadWriteArray(ref reader, ref writer);
+                            break;
+                    }
+                }
+            }
+        }
+
+        private void ReadWritePropertyNameTargets(ref Utf8JsonReader reader, ref Utf8JsonWriter writer)
+        {
+            Debug.Assert(reader.GetStringValue() == "targets");
+
+            reader.Read();
+            Debug.Assert(reader.TokenType == JsonTokenType.StartObject);
+
+            writer.WriteStartObject("targets", escape: false);
+
+            reader.Read();
+            if (reader.TokenType == JsonTokenType.PropertyName)
+            {
+                string propertyName = reader.GetStringValue();
+                reader.Read();
+                Debug.Assert(reader.TokenType == JsonTokenType.StartObject);
+                writer.WriteStartObject(propertyName);
+
+                reader.Read();
+                if (reader.TokenType == JsonTokenType.PropertyName)
+                {
+                    string targetLibrary = reader.GetStringValue();
+                    _version = targetLibrary.Substring(targetLibrary.IndexOf('/') + 1);
+                    if (!string.IsNullOrEmpty(NewName))
+                    {
+                        string newPropertyName = NewName + '/' + _version;
+                        ReadWritePropertyName(newPropertyName, ref reader, ref writer);
+                    }
+                    else
+                    {
+                        Skip(ref reader);
+                    }
+                }
+                else
+                {
+                    Debug.Assert(reader.TokenType == JsonTokenType.EndObject);
+                    writer.WriteEndObject();
+                }
+            }
+            else
+            {
+                Debug.Assert(reader.TokenType == JsonTokenType.EndObject);
+                writer.WriteEndObject();
+            }
+        }
+
+        private void ReadWritePropertyNameLibraries(ref Utf8JsonReader reader, ref Utf8JsonWriter writer)
+        {
+            Debug.Assert(reader.GetStringValue() == "libraries");
+            Debug.Assert(!string.IsNullOrEmpty(_version));
+
+            reader.Read();
+            Debug.Assert(reader.TokenType == JsonTokenType.StartObject);
+
+            writer.WriteStartObject("libraries", escape: false);
+
+            reader.Read();
+            if (reader.TokenType == JsonTokenType.PropertyName)
+            {
+                if (!string.IsNullOrEmpty(NewName))
+                {
+                    string newPropertyName = NewName + '/' + _version;
+                    ReadWritePropertyName(newPropertyName, ref reader, ref writer);
+                }
+                else
+                {
+                    Skip(ref reader);
+                }
+            }
+            else
+            {
+                Debug.Assert(reader.TokenType == JsonTokenType.EndObject);
+                writer.WriteEndObject();
+            }
+        }
+
+        private void ReadWriteArray(ref Utf8JsonReader reader, ref Utf8JsonWriter writer)
+        {
+            while (reader.Read())
+            {
+                switch (reader.TokenType)
+                {
+                    case JsonTokenType.True:
+                        writer.WriteBooleanValue(true);
+                        break;
+                    case JsonTokenType.False:
+                        writer.WriteBooleanValue(false);
+                        break;
+                    case JsonTokenType.Number:
+                        throw CreateUnexpectedException(ref reader, "Expect non-numeric JSON tokens only within deps.json.");
+                    case JsonTokenType.String:
+                        writer.WriteStringValue(reader.ValueSpan);
+                        break;
+                    case JsonTokenType.Null:
+                        writer.WriteNullValue();
+                        break;
+                    case JsonTokenType.StartObject:
+                        writer.WriteStartObject();
+                        ReadWriteObject(ref reader, ref writer);
+                        break;
+                    case JsonTokenType.StartArray:
+                        writer.WriteStartArray();
+                        ReadWriteArray(ref reader, ref writer);
+                        break;
+                    case JsonTokenType.EndArray:
+                        writer.WriteEndArray();
+                        return;
+                }
+            }
+        }
+    }
+}
---
+++ tools-local/tasks/StreamBufferWriter.Netstandard2_0.cs
@@ -0,0 +1,80 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Buffers;
+using System.Diagnostics;
+using System.IO;
+
+namespace Microsoft.DotNet.Build.Tasks
+{
+    internal struct StreamBufferWriter : IBufferWriter<byte>, IDisposable
+    {
+        Stream _stream;
+        byte[] _rentedBuffer;
+
+        public StreamBufferWriter(Stream stream, int bufferSize = 256)
+        {
+            if (bufferSize <= 0)
+            {
+                throw new ArgumentNullException(nameof(bufferSize));
+            }
+            _stream = stream ?? throw new ArgumentNullException(nameof(stream));
+
+            _rentedBuffer = ArrayPool<byte>.Shared.Rent(bufferSize);
+        }
+
+        public void Advance(int count)
+        {
+            _stream.Write(_rentedBuffer, 0, count);
+            _rentedBuffer.AsSpan(0, count).Clear();
+        }
+
+        /// <summary>
+        /// Returns rented buffer back to the pool
+        /// </summary>
+        public void Dispose()
+        {
+            _rentedBuffer.AsSpan().Clear();
+            ArrayPool<byte>.Shared.Return(_rentedBuffer);
+            _rentedBuffer = null;
+        }
+
+        public Memory<byte> GetMemory(int sizeHint = 0)
+        {
+            if (sizeHint > _rentedBuffer.Length)
+            {
+                var newSize = _rentedBuffer.Length * 2;
+                if (sizeHint != 0)
+                {
+                    newSize = sizeHint;
+                }
+                var temp = _rentedBuffer;
+                _rentedBuffer = ArrayPool<byte>.Shared.Rent(newSize);
+                temp.AsSpan().Clear();
+                ArrayPool<byte>.Shared.Return(temp);
+            }
+            Debug.Assert(_rentedBuffer.Length > 0);
+            return _rentedBuffer;
+        }
+
+        public Span<byte> GetSpan(int sizeHint = 0)
+        {
+            if (sizeHint > _rentedBuffer.Length)
+            {
+                var newSize = _rentedBuffer.Length * 2;
+                if (sizeHint != 0)
+                {
+                    newSize = sizeHint;
+                }
+                var temp = _rentedBuffer;
+                _rentedBuffer = ArrayPool<byte>.Shared.Rent(newSize);
+                temp.AsSpan().Clear();
+                ArrayPool<byte>.Shared.Return(temp);
+            }
+            Debug.Assert(_rentedBuffer.Length > 0);
+            return _rentedBuffer;
+        }
+    }
+}
