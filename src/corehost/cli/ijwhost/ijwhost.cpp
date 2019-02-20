d5f87e5b3581447df7c1af6172c38211bf74a901
Jeremy Koritzinsky <jekoritz@microsoft.com>
2019-02-19 16:52:04 -0800

    clean up ijwhost.cpp since we only have one entrypoint into hostfxr from ijwhost now.

--- src/corehost/cli/ijwhost/ijwhost.cpp
+++ src/corehost/cli/ijwhost/ijwhost.cpp
@@ -13,65 +13,55 @@
 
 #if defined(_WIN32)
 // IJW entry points are defined without the __declspec(dllexport) attribute.
-// The issue here is that the MSVC compiler links to the exact names _CorExeMain and _CorDllMain instead of their stdcall-managled names.
-// So we need to export their exact names, which __declspec(dllexport) doesn't do. The solution here is to the use a .def file on Windows.
+// The issue here is that the MSVC compiler links to the exact name _CorDllMain instead of their stdcall-managled names.
+// So we need to export the exact name, which __declspec(dllexport) doesn't do. The solution here is to the use a .def file on Windows.
 #define IJW_API extern "C"
 #else
 #define IJW_API SHARED_API
 #endif // _WIN32
 
-using load_in_memory_assembly_fn = void(*)(pal::dll_t handle, const pal::char_t* path);
-
-namespace
-{
-    int load_hostfxr_delegate(pal::dll_t moduleHandle, const char* entryPoint, void** delegate)
-    {
-        pal::dll_t fxr;
-
-        pal::string_t host_path;
-        if (!pal::get_own_module_path(&host_path) || !pal::realpath(&host_path))
-        {
-            trace::error(_X("Failed to resolve full path of the current host module [%s]"), host_path.c_str());
-            return StatusCode::CoreHostCurHostFindFailure;
-        }
-
-        pal::string_t dotnet_root;
-        pal::string_t fxr_path;
-        if (!resolve_fxr_path(get_directory(host_path), &dotnet_root, &fxr_path))
-        {
-            return StatusCode::CoreHostLibMissingFailure;
-        }
-
-        // Load library
-        if (!pal::load_library(&fxr_path, &fxr))
-        {
-            trace::error(_X("The library %s was found, but loading it from %s failed"), LIBFXR_NAME, fxr_path.c_str());
-            trace::error(_X("  - Installing .NET Core prerequisites might help resolve this problem."));
-            trace::error(_X("     %s"), DOTNET_CORE_INSTALL_PREREQUISITES_URL);
-            return StatusCode::CoreHostLibLoadFailure;
-        }
-
-        // Leak fxr
-
-        auto get_delegate_from_hostfxr = (hostfxr_get_delegate_fn)pal::get_symbol(fxr, entryPoint);
-        if (get_delegate_from_hostfxr == nullptr)
-            return StatusCode::CoreHostEntryPointFailure;
-
-        pal::string_t app_path;
-
-        if (!pal::get_module_path(moduleHandle, &app_path))
-        {
-            trace::error(_X("Failed to resolve full path of the current mixed-mode module [%s]"), host_path.c_str());
-            return StatusCode::LibHostCurExeFindFailure;
-        }
-
-        return get_delegate_from_hostfxr(host_path.c_str(), dotnet_root.c_str(), app_path.c_str(), delegate);
-    }
-}
-
 pal::hresult_t get_load_in_memory_assembly_delegate(pal::dll_t handle, load_in_memory_assembly_fn* delegate)
 {
-    return load_hostfxr_delegate(handle, "hostfxr_get_load_in_memory_assembly_delegate", (void**)delegate);
+    pal::dll_t fxr;
+
+    pal::string_t host_path;
+    if (!pal::get_own_module_path(&host_path) || !pal::realpath(&host_path))
+    {
+        trace::error(_X("Failed to resolve full path of the current host module [%s]"), host_path.c_str());
+        return StatusCode::CoreHostCurHostFindFailure;
+    }
+
+    pal::string_t dotnet_root;
+    pal::string_t fxr_path;
+    if (!resolve_fxr_path(get_directory(host_path), &dotnet_root, &fxr_path))
+    {
+        return StatusCode::CoreHostLibMissingFailure;
+    }
+
+    // Load library
+    if (!pal::load_library(&fxr_path, &fxr))
+    {
+        trace::error(_X("The library %s was found, but loading it from %s failed"), LIBFXR_NAME, fxr_path.c_str());
+        trace::error(_X("  - Installing .NET Core prerequisites might help resolve this problem."));
+        trace::error(_X("     %s"), DOTNET_CORE_INSTALL_PREREQUISITES_URL);
+        return StatusCode::CoreHostLibLoadFailure;
+    }
+
+    // Leak fxr
+
+    auto get_delegate_from_hostfxr = (hostfxr_get_delegate_fn)pal::get_symbol(fxr, "hostfxr_get_load_in_memory_assembly_delegate");
+    if (get_delegate_from_hostfxr == nullptr)
+        return StatusCode::CoreHostEntryPointFailure;
+
+    pal::string_t app_path;
+
+    if (!pal::get_module_path(handle, &app_path))
+    {
+        trace::error(_X("Failed to resolve full path of the current mixed-mode module [%s]"), host_path.c_str());
+        return StatusCode::LibHostCurExeFindFailure;
+    }
+    
+    return get_delegate_from_hostfxr(host_path.c_str(), dotnet_root.c_str(), app_path.c_str(), (void**)delegate);
 }
 
 IJW_API BOOL STDMETHODCALLTYPE _CorDllMain(HINSTANCE hInst,
