0510a5a4216590dee5b96eb0e4f2de3e22a18347
vitek-karas <vitek.karas@microsoft.com>
2019-02-21 04:57:36 -0800

    Framework resolution tests
    Backfill detailed tests for framework resolution behavior, specifically
    around roll-forward.
    
    Introduces some new test helpers
    * hostpolicy mock, to avoid actually loading and running coreclr just to
    validate selected frameworks
    * Builder-like API for `.runtimeconfig.js`
    * Builder-like API for shared frameworks
    * Some helper classes to limit file copy while keeping the tests fully
    isolated

---
+++ src/test/HostActivationTests/FrameworkResolution/ApplyPatchesSettings.cs
@@ -0,0 +1,160 @@
+﻿// Copyright (c) .NET Foundation and contributors. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Microsoft.DotNet.Cli.Build;
+using Microsoft.DotNet.Cli.Build.Framework;
+using System;
+using Xunit;
+
+namespace Microsoft.DotNet.CoreSetup.Test.HostActivation.FrameworkResolution
+{
+    public class ApplyPatchesSettings :
+        FrameworkResolutionBase,
+        IClassFixture<ApplyPatchesSettings.SharedTestState>
+    {
+        private SharedTestState SharedState { get; }
+
+        public ApplyPatchesSettings(SharedTestState sharedState)
+        {
+            SharedState = sharedState;
+        }
+
+        [Fact]
+        public void Default()
+        {
+            RunTest(
+                runtimeConfig => runtimeConfig
+                    .WithFramework(MicrosoftNETCoreApp, "5.1.2"),
+                result => result.Should().Pass()
+                    .And.HaveResolvedFrameworkVersion("5.1.3"));
+        }
+
+        [Fact]
+        public void RuntimeConfigOnly()
+        {
+            RunTest(
+                runtimeConfig => runtimeConfig
+                    .WithFramework(MicrosoftNETCoreApp, "5.1.2")
+                    .WithApplyPatches(false),
+                result => result.Should().Pass()
+                    .And.HaveResolvedFrameworkVersion("5.1.2"));
+        }
+
+        [Fact]
+        public void FrameworkReferenceOnly()
+        {
+            RunTest(
+                runtimeConfig => runtimeConfig
+                    .WithFramework(new RuntimeConfig.Framework(MicrosoftNETCoreApp, "5.1.2")
+                        .WithApplyPatches(false)),
+                result => result.Should().Pass()
+                    .And.HaveResolvedFrameworkVersion("5.1.2"));
+        }
+
+        [Fact]
+        public void Priority()
+        {
+            RunTest(
+                runtimeConfig => runtimeConfig
+                    .WithFramework(new RuntimeConfig.Framework(MicrosoftNETCoreApp, "5.1.2")
+                        .WithApplyPatches(false))
+                    .WithApplyPatches(true),
+                result => result.Should().Pass()
+                    .And.HaveResolvedFrameworkVersion("5.1.2"));
+
+            RunTest(
+                runtimeConfig => runtimeConfig
+                    .WithFramework(new RuntimeConfig.Framework(MicrosoftNETCoreApp, "5.1.2")
+                        .WithApplyPatches(true))
+                    .WithApplyPatches(false),
+                result => result.Should().Pass()
+                    .And.HaveResolvedFrameworkVersion("5.1.3"));
+        }
+
+        [Fact]
+        public void InnerFrameworkReference_RuntimeConfig()
+        {
+            using (var dotnetCustomizer = SharedState.DotNetWithFrameworks.Customize())
+            {
+                dotnetCustomizer.Framework("MiddleWare").RuntimeConfig(runtimeConfig =>
+                    runtimeConfig.WithApplyPatches(false));
+
+                RunTest(
+                    runtimeConfig => runtimeConfig
+                        .WithFramework("MiddleWare", "2.1.0"),
+                    result => result.Should().Pass()
+                        .And.HaveResolvedFrameworkVersion("5.1.2")
+                        .And.HaveResolvedFrameworkVersion("2.1.2"));
+            }
+        }
+
+        [Fact]
+        public void InnerFrameworkReference_Framework()
+        {
+            using (var dotnetCustomizer = SharedState.DotNetWithFrameworks.Customize())
+            {
+                dotnetCustomizer.Framework("MiddleWare").RuntimeConfig(runtimeConfig =>
+                    runtimeConfig.GetFramework(MicrosoftNETCoreApp).WithApplyPatches(false));
+
+                RunTest(
+                    runtimeConfig => runtimeConfig
+                        .WithFramework("MiddleWare", "2.1.0"),
+                    result => result.Should().Pass()
+                        .And.HaveResolvedFrameworkVersion("5.1.2")
+                        .And.HaveResolvedFrameworkVersion("2.1.2"));
+            }
+        }
+
+        [Fact]
+        public void NoInheritance()
+        {
+            RunTest(
+                runtimeConfig => runtimeConfig
+                    .WithFramework("MiddleWare", "2.1.2")
+                    .WithApplyPatches(false),
+                result => result.Should().Pass()
+                    .And.HaveResolvedFrameworkVersion("5.1.3"));
+
+            RunTest(
+                runtimeConfig => runtimeConfig
+                    .WithFramework(new RuntimeConfig.Framework("MiddleWare", "2.1.2")
+                        .WithApplyPatches(false)),
+                result => result.Should().Pass()
+                    .And.HaveResolvedFrameworkVersion("5.1.3"));
+        }
+
+        private void RunTest(
+            Func<RuntimeConfig, RuntimeConfig> runtimeConfig,
+            Action<CommandResult> resultAction,
+            string[] environment = null,
+            string[] commandLine = null)
+        {
+            RunTest(
+                SharedState.DotNetWithFrameworks,
+                SharedState.FrameworkReferenceApp,
+                runtimeConfig,
+                resultAction,
+                environment,
+                commandLine);
+        }
+
+        public class SharedTestState : SharedTestStateBase
+        {
+            public TestApp FrameworkReferenceApp { get; }
+
+            public DotNetCli DotNetWithFrameworks { get; }
+
+            public SharedTestState()
+            {
+                DotNetWithFrameworks = DotNet("WithOneFramework")
+                    .AddMicrosoftNETCoreAppFramework("5.1.2")
+                    .AddMicrosoftNETCoreAppFramework("5.1.3")
+                    .AddFramework("MiddleWare", "2.1.2", runtimeConfig =>
+                        runtimeConfig.WithFramework(MicrosoftNETCoreApp, "5.1.2"))
+                    .Build();
+
+                FrameworkReferenceApp = CreateFrameworkReferenceApp();
+            }
+        }
+    }
+}
