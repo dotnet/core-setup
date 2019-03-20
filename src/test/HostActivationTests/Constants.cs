47e5963b29560db010b361cae6f739bbb1675c39
vitek-karas <vitek.karas@microsoft.com>
2019-03-20 00:57:02 -0700

    First

--- src/test/HostActivationTests/Constants.cs
+++ src/test/HostActivationTests/Constants.cs
@@ -1,6 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿// Copyright (c) .NET Foundation and contributors. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 namespace Microsoft.DotNet.CoreSetup.Test.HostActivation
 {
@@ -16,6 +15,20 @@ namespace Microsoft.DotNet.CoreSetup.Test.HostActivation
             public const string RuntimeConfigPropertyName = "rollForwardOnNoCandidateFx";
             public const string CommandLineArgument = "--roll-forward-on-no-candidate-fx";
             public const string EnvironmentVariable = "DOTNET_ROLL_FORWARD_ON_NO_CANDIDATE_FX";
+        }
+
+        public static class RollForwardSetting
+        {
+            public const string RuntimeConfigPropertyName = "rollForward";
+            public const string CommandLineArgument = "--roll-forward";
+            public const string EnvironmentVariable = "DOTNET_ROLL_FORWARD";
+
+            public const string LatestPatch = "LatestPatch";
+            public const string Minor = "Minor";
+            public const string Major = "Major";
+            public const string LatestMinor = "LatestMinor";
+            public const string LatestMajor = "LatestMajor";
+            public const string Disable = "Disable";
         }
 
         public static class FxVersion
---
+++ src/test/HostActivationTests/FrameworkResolution/RollForwardSettings.cs
@@ -0,0 +1,85 @@
+﻿// Copyright (c) .NET Foundation and contributors. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Microsoft.DotNet.Cli.Build;
+using Microsoft.DotNet.Cli.Build.Framework;
+using System;
+using Xunit;
+
+namespace Microsoft.DotNet.CoreSetup.Test.HostActivation.FrameworkResolution
+{
+    public class RollForwardSettings :
+        FrameworkResolutionBase,
+        IClassFixture<RollForwardSettings.SharedTestState>
+    {
+        private const string MiddleWare = "MiddleWare";
+
+        private SharedTestState SharedState { get; }
+
+        public RollForwardSettings(SharedTestState sharedState)
+        {
+            SharedState = sharedState;
+        }
+
+        [Fact]
+        public void Default()
+        {
+            RunTest(
+                runtimeConfig => runtimeConfig
+                    .WithFramework(MicrosoftNETCoreApp, "4.0.0"),
+                result => result.Should().Fail()
+                    .And.DidNotFindCompatibleFrameworkVersion());
+
+            RunTest(
+                runtimeConfig => runtimeConfig
+                    .WithFramework(MicrosoftNETCoreApp, "5.0.0"),
+                result => result.Should().Pass()
+                    .And.HaveResolvedFramework(MicrosoftNETCoreApp, "5.1.3"));
+        }
+
+        [Fact]
+        public void RuntimeConfigOnly()
+        {
+            RunTest(
+                runtimeConfig => runtimeConfig
+                    .WithRollForward(Constants.RollForwardSetting.Major)
+                    .WithFramework(MicrosoftNETCoreApp, "4.0.0"),
+                result => result.Should().Pass()
+                    .And.HaveResolvedFramework(MicrosoftNETCoreApp, "5.1.3"));
+        }
+
+        private void RunTest(
+            Func<RuntimeConfig, RuntimeConfig> runtimeConfig,
+            Action<CommandResult> resultAction,
+            string[] environment = null,
+            string[] commandLine = null)
+        {
+            RunTest(
+                SharedState.DotNetWithFrameworks,
+                SharedState.FrameworkReferenceApp,
+                runtimeConfig,
+                resultAction,
+                environment,
+                commandLine);
+        }
+
+        public class SharedTestState : SharedTestStateBase
+        {
+            public TestApp FrameworkReferenceApp { get; }
+
+            public DotNetCli DotNetWithFrameworks { get; }
+
+            public SharedTestState()
+            {
+                DotNetWithFrameworks = DotNet("WithOneFramework")
+                    .AddMicrosoftNETCoreAppFramework("5.1.3")
+                    .AddFramework(
+                        MiddleWare, "2.1.2",
+                        runtimeConfig => runtimeConfig.WithFramework(MicrosoftNETCoreApp, "5.1.3"))
+                    .Build();
+
+                FrameworkReferenceApp = CreateFrameworkReferenceApp();
+            }
+        }
+    }
+}
