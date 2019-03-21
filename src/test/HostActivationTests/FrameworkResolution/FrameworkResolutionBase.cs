60925e30c22c11e84b2086e37fd7ae48a70e6d08
Vitek Karas <vitek.karas@microsoft.com>
2019-03-21 08:30:22 -0700

    Generalize test settings for framework resolution tests
    Add ability to easily apply settings (roll forward and so on) in a dynamic way (picking location via variable).
    This simplifies testing of settings combinations.

--- src/test/HostActivationTests/FrameworkResolution/FrameworkResolutionBase.cs
+++ src/test/HostActivationTests/FrameworkResolution/FrameworkResolutionBase.cs
@@ -11,7 +11,7 @@ using System.Linq;
 
 namespace Microsoft.DotNet.CoreSetup.Test.HostActivation.FrameworkResolution
 {
-    public abstract class FrameworkResolutionBase
+    public abstract partial class FrameworkResolutionBase
     {
         protected const string MicrosoftNETCoreApp = "Microsoft.NETCore.App";
 
@@ -24,23 +24,85 @@ namespace Microsoft.DotNet.CoreSetup.Test.HostActivation.FrameworkResolution
             string[] commandLine = null,
             bool multiLevelLookup = false)
         {
-            runtimeConfig(RuntimeConfig.Path(app.RuntimeConfigJson)).Save();
+            RunTest(
+                dotnet,
+                app,
+                new TestSettings()
+                {
+                    RuntimeConfigCustomizer = runtimeConfig,
+                    Environment = environment,
+                    CommandLine = commandLine
+                },
+                resultAction,
+                multiLevelLookup);
+        }
 
-            IEnumerable<string> args = commandLine ?? Enumerable.Empty<string>();
-            args = args.Concat(new string[] { app.AppDll });
+        public class TestSettings
+        {
+            public Func<RuntimeConfig, RuntimeConfig> RuntimeConfigCustomizer { get; set; }
+            public IEnumerable<string> Environment { get; set; }
+            public IEnumerable<string> CommandLine { get; set; }
+
+            public TestSettings WithRuntimeConfigCustomizer(Func<RuntimeConfig, RuntimeConfig> customizer)
+            {
+                Func<RuntimeConfig, RuntimeConfig> previousCustomizer = RuntimeConfigCustomizer;
+                if (previousCustomizer == null)
+                {
+                    RuntimeConfigCustomizer = customizer;
+                }
+                else
+                {
+                    RuntimeConfigCustomizer = runtimeConfig => customizer(previousCustomizer(runtimeConfig));
+                }
+
+                return this;
+            }
+
+            public TestSettings WithEnvironment(string key, string value)
+            {
+                string [] env = new string[] { key + "=" + value };
+                Environment = Environment == null ? env : Environment.Concat(env);
+                return this;
+            }
+
+            public TestSettings WithCommandLine(params string[] args)
+            {
+                CommandLine = CommandLine == null ? args : CommandLine.Concat(args);
+                return this;
+            }
+
+            public TestSettings With(Func<TestSettings, TestSettings> modifier)
+            {
+                return modifier(this);
+            }
+        }
+
+        protected void RunTest(
+            DotNetCli dotnet,
+            TestApp app,
+            TestSettings settings,
+            Action<CommandResult> resultAction,
+            bool multiLevelLookup = false)
+        {
+            if (settings.RuntimeConfigCustomizer != null)
+            {
+                settings.RuntimeConfigCustomizer(RuntimeConfig.Path(app.RuntimeConfigJson)).Save();
+            }
+
+            settings.WithCommandLine(app.AppDll);
 
             IDictionary<string, string> environmentVariables = null;
-            if (environment != null)
+            if (settings.Environment != null)
             {
                 environmentVariables = new Dictionary<string, string>();
-                foreach (string v in environment)
+                foreach (string v in settings.Environment)
                 {
                     string[] s = v.Split('=');
                     environmentVariables.Add(s[0], s[1]);
                 }
             }
 
-            CommandResult result = dotnet.Exec(args.First(), args.Skip(1).ToArray())
+            CommandResult result = dotnet.Exec(settings.CommandLine.First(), settings.CommandLine.Skip(1).ToArray())
                 .EnvironmentVariable("COREHOST_TRACE", "1")
                 .EnvironmentVariable("DOTNET_MULTILEVEL_LOOKUP", multiLevelLookup ? "1" : "0")
                 .Environment(environmentVariables)
