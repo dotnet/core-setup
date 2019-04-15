adb2ceb722336bb5cfdbe1a6d1192fe176fde66e
wtgodbe <wigodbe@microsoft.com>
2019-04-15 16:52:04 -0700

    Update dependencies for 1.1.13

--- build_projects/shared-build-targets-utils/DependencyVersions.cs
+++ build_projects/shared-build-targets-utils/DependencyVersions.cs
@@ -7,7 +7,7 @@ namespace Microsoft.DotNet.Cli.Build
 {
     public class DependencyVersions
     {
-        public static readonly string CoreCLRVersion = "1.1.12";
-        public static readonly string JitVersion = "1.1.12";
+        public static readonly string CoreCLRVersion = "1.1.13-servicing-27612-03";
+        public static readonly string JitVersion = "1.1.13-servicing-27612-03";
     }
 }
