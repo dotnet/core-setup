6e6f911888c89e0b23b217bfe27a8e2303b089f3
Elinor Fung <47805090+elinor-fung@users.noreply.github.com>
2019-04-24 12:53:34 -0700

    Apply suggestions from code review
    Co-Authored-By: vitek-karas <vitek.karas@microsoft.com>

--- src/corehost/common/utils.h
+++ src/corehost/common/utils.h
@@ -79,9 +79,9 @@ private:
 public:
     propagate_error_writer_t(set_error_writer_fn set_error_writer)
     {
-        // Previous trace messages from the caller module must be printed before calling trace::setup in calee module
+        // Previous trace messages from the caller module must be printed before calling trace::setup in callee module
         // The two modules have different trace util instances and thus don't share file IO buffers
-        // not flashing may lead to writing traces from before the call after the call due to module mismatch.
+        // Not flushing may lead to traces from before the call being written after the call due to module mismatch.
         trace::flush();
 
         m_set_error_writer = set_error_writer;
