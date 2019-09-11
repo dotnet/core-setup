aa3b6eb9c30c77524f8df9dfa84d444f5b6a66c8
Davis Goodin <dagood@microsoft.com>
2019-09-11 10:11:07 -0500

    Improve task code structure for readability

--- tools-local/tasks/CopyNupkgAndChangeVersion.cs
+++ tools-local/tasks/CopyNupkgAndChangeVersion.cs
@@ -36,77 +36,102 @@ namespace Microsoft.DotNet.Build.Tasks
 
             using (ZipArchive zip = ZipFile.Open(TargetFile, ZipArchiveMode.Update))
             {
-                foreach (var nuspec in zip.Entries.Where(e => e.FullName.EndsWith(".nuspec")))
-                {
-                    Rewrite(nuspec, s =>
-                    {
-                        XDocument content = XDocument.Parse(s);
-
-                        XNamespace rootNamespace = content.Root.GetDefaultNamespace();
-                        XName GetQualifiedName(string name) => rootNamespace.GetName(name);
-
-                        var versionElement = content
-                            .Element(GetQualifiedName("package"))
-                            .Element(GetQualifiedName("metadata"))
-                            .Element(GetQualifiedName("version"));
-
-                        if (versionElement.Value != OriginalVersion)
-                        {
-                            Log.LogError(
-                                $"Original version is '{versionElement.Value}', " +
-                                $"expected '{OriginalVersion}'");
-                        }
-
-                        versionElement.Value = TargetVersion;
-
-                        foreach (var dependency in content
-                            .Descendants(GetQualifiedName("dependency"))
-                            .Where(x =>
-                                x.Attribute("version").Value == OriginalVersion &&
-                                DependencyPackageIdsToChange?.Contains(x.Attribute("id").Value) == true))
-                        {
-                            dependency.Value = TargetVersion;
-                        }
-
-                        return content.ToString();
-                    });
-                }
-
-                foreach (var runtimeJson in zip.Entries.Where(e => e.FullName == "runtime.json"))
-                {
-                    Rewrite(runtimeJson, s =>
-                    {
-                        JObject content = JObject.Parse(s);
-                        var versionProperties = content
-                            .Descendants()
-                            .OfType<JProperty>()
-                            .Where(p =>
-                                p.Value is JValue v &&
-                                v.Type == JTokenType.String);
-
-                        foreach (var p in versionProperties)
-                        {
-                            var range = VersionRange.Parse(p.Value.Value<string>());
-
-                            if (range.MinVersion.OriginalVersion == OriginalVersion)
-                            {
-                                var newRange = new VersionRange(
-                                    NuGetVersion.Parse(TargetVersion),
-                                    range.Float);
-
-                                p.Value = newRange.ToString();
-                            }
-                        }
-
-                        return content.ToString();
-                    });
-                }
+                RewriteNuspec(zip);
+                RewriteRuntimeJson(zip);
             }
 
             return !Log.HasLoggedErrors;
         }
 
-        private void Rewrite(ZipArchiveEntry entry, Func<string, string> rewrite)
+        private void RewriteNuspec(ZipArchive zip)
+        {
+            foreach (var nuspec in zip.Entries.Where(e => e.FullName.EndsWith(".nuspec")))
+            {
+                Rewrite(nuspec, s =>
+                {
+                    XDocument content = XDocument.Parse(s);
+
+                    RewriteNuspecPackageVersion(content);
+                    RewriteNuspecDependencyVersions(content);
+
+                    return content.ToString();
+                });
+            }
+        }
+
+        private void RewriteRuntimeJson(ZipArchive zip)
+        {
+            foreach (var runtimeJson in zip.Entries.Where(e => e.FullName == "runtime.json"))
+            {
+                Rewrite(runtimeJson, s =>
+                {
+                    JObject content = JObject.Parse(s);
+
+                    RewriteRuntimeJsonVersions(content);
+
+                    return content.ToString();
+                });
+            }
+        }
+
+        private void RewriteNuspecPackageVersion(XDocument content)
+        {
+            XElement versionElement = content
+                .Element(CreateQualifiedName(content, "package"))
+                .Element(CreateQualifiedName(content, "metadata"))
+                .Element(CreateQualifiedName(content, "version"));
+
+            if (versionElement.Value != OriginalVersion)
+            {
+                Log.LogError(
+                    $"Original version is '{versionElement.Value}', " +
+                    $"expected '{OriginalVersion}'");
+            }
+
+            versionElement.Value = TargetVersion;
+        }
+
+        private void RewriteNuspecDependencyVersions(XDocument content)
+        {
+            foreach (var dependency in content
+                .Descendants(CreateQualifiedName(content, "dependency"))
+                .Where(x =>
+                    x.Attribute("version").Value == OriginalVersion &&
+                    DependencyPackageIdsToChange?.Contains(x.Attribute("id").Value) == true))
+            {
+                dependency.Value = TargetVersion;
+            }
+        }
+
+        private void RewriteRuntimeJsonVersions(JObject content)
+        {
+            var versionProperties = content
+                .Descendants()
+                .OfType<JProperty>()
+                .Where(p =>
+                    p.Value is JValue v &&
+                    v.Type == JTokenType.String);
+
+            foreach (var p in versionProperties)
+            {
+                var range = VersionRange.Parse(p.Value.Value<string>());
+
+                if (range.MinVersion.OriginalVersion == OriginalVersion)
+                {
+                    var newRange = new VersionRange(
+                        NuGetVersion.Parse(TargetVersion),
+                        range.Float);
+
+                    p.Value = newRange.ToString();
+                }
+            }
+        }
+        private static XName CreateQualifiedName(XDocument doc, string name)
+        {
+            return doc.Root.GetDefaultNamespace().GetName(name);
+        }
+
+        private static void Rewrite(ZipArchiveEntry entry, Func<string, string> rewrite)
         {
             using (var stream = entry.Open())
             using (var reader = new StreamReader(stream))
