07553490703fe9245da709b682ed4d4a00729855
Jeremy Koritzinsky <jekoritz@microsoft.com>
2019-09-18 16:07:06 -0700

    Copy files over from dotnet/sdk.

---
+++ src/managed/Microsoft.NET.HostModel/ComHost/ConflictingGuidException.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Microsoft.NET.HostModel.ComHost
+{
+    class ConflictingGuidException : Exception
+    {
+        public ConflictingGuidException(string typeName1, string typeName2, Guid guid)
+        {
+            TypeName1 = typeName1;
+            TypeName2 = typeName2;
+            Guid = guid;
+        }
+
+        public string TypeName1 { get; }
+        public string TypeName2 { get; }
+        public Guid Guid { get; }
+    }
+}
---
+++ src/managed/Microsoft.NET.HostModel/ComHost/MissingGuidException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Microsoft.NET.HostModel.ComHost
+{
+    class MissingGuidException : Exception
+    {
+        public MissingGuidException(string typeName)
+        {
+            TypeName = typeName;
+        }
+
+        public string TypeName { get; }
+    }
+}
