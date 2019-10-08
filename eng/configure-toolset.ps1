05e30fc9c17b27377353924b3b3a1ce3f28aa6fd
Viktor Hofer <viktor.hofer@microsoft.com>
2019-10-08 11:13:07 +0200

    Use the user's nuget cache for local builds
    Other dotnet repositories like CoreFx are already using the user's
    nuget cache for local builds vs the repo local cache for CI builds
    and no problems were reported. Switching to remove differences in builds

--- eng/configure-toolset.ps1
+++
@@ -1,5 +0,0 @@
-# Always use the local repo packages directory instead of the user's NuGet cache
-# to keep the same between "ci" and non-"ci" builds. If the efficiency gain is
-# required and it's worth maintaining the different types of build, this can be
-# removed.
-$script:useGlobalNuGetCache = $false
